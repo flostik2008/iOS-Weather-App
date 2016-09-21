9f37d27824dd27dbf98329bfc635a3d6484a7520
Evgeny Vlasov <564n@Evgenys-MacBook-Pro.local>
2016-09-21 01:33:17 -0700

    Current weather updates.

---
+++ Weather-App/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Weather-App
+//
+//  Created by Evgeny Vlasov on 9/19/16.
+//  Copyright © 2016 Evgeny Vlasov. All rights reserved.
+//
+
+import Foundation
---
+++ Weather-App/CurrentWeather.swift
@@ -0,0 +1,9 @@
+//
+//  CurrentWeather.swift
+//  Weather-App
+//
+//  Created by Evgeny Vlasov on 9/20/16.
+//  Copyright © 2016 Evgeny Vlasov. All rights reserved.
+//
+
+import Foundation
